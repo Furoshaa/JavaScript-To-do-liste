d13c3122771d5dbd787140f40344ed4466a064a1
Furosha <levent.kyllian@gmail.com>
2025-01-06 22:42:44 +0100

    creation de la base du tableau en js + deplacement de la balise script pout quelle passe bien apres que le DOM soit chargé

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+// Tableau des tâches
 let todo = [];
 
+// Fonction pour ajouter une tâche
 function addTodo(buttonAdd) {
 
 };
 
+// Event listener pour ajouter une tâche quand button click
 document.getElementById('buttonAdd').addEventListener('click', addTodo);
+
+// Création du tableau
+const table = document.createElement('table');
+const tr = document.createElement('tr');
+const td = document.createElement('td');
+
+// Appliquer la largeur à la cellule
+table.style.border = '1px solid black';
+td.style.width = '10px';
+
+// Ajouter les elements les uns aux autres
+tr.appendChild(td);
+table.appendChild(tr);
+document.body.appendChild(table);
