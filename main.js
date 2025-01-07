6ae5d200d6d1ee2124d6711229479c1689eb659a
Furosha <levent.kyllian@gmail.com>
2025-01-07 01:11:59 +0100

    les indication dans le tp sont vraiment impossible a suivre. Implementation du javascript selon les indication du tp

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function addTodo() {
     if (newTache !== "") {
         todoListe.push(newTache);
         document.getElementById('input').value = "";
+        addTodoToTable(newTache);
     }
     else {
         alert("Veuillez saisir une tache");
@@ -15,19 +16,59 @@ function addTodo() {
 
 }
 
+// Fonction pour ajouter la tache au tableau HTML
+function addTodoToTable(newTache) {
+
+    let table = document.querySelector('table');
+
+    if (!table) {
+        //Creation du tableau HTML
+        table = document.createElement('table');
+        table.style.border = '1px solid black';
+        document.body.appendChild(table);
+
+        //Creation de la caption du tableau
+        const caption = document.createElement('caption');
+        caption.textContent = 'Liste des tâches';
+        table.appendChild(caption);
+
+        // Ajout des taches dans le tableau
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const th2 = document.createElement('th');
+
+        th.textContent = 'Numéro';
+        th2.textContent = 'Libellé';
+
+        th.style.width = '10px';
+
+        table.appendChild(tr);
+        tr.appendChild(th);
+        tr.appendChild(th2);
+
+        const tbody = document.createElement('tbody');
+        table.appendChild(tbody);
+
+        const tr2 = document.createElement('tr');
+        const td = document.createElement('td');
+        const td2 = document.createElement('td');
+        tbody.appendChild(tr2);
+        tr2.appendChild(td);
+        tr2.appendChild(td2);
+
+        tbody.style.display = 'flex';
+        tbody.style.justifyContent = 'center';  // Centrer horizontalement
+        tbody.style.alignItems = 'center';      // Centrer verticalement
+
+        td.textContent = todoListe.length;
+
+        td2.textContent = newTache;
+
+    } else {
+        console.log("table de base : ", table);
+    };
+
+};
+
 // Event listener pour ajouter une tache quand button click
 document.getElementById('buttonAdd').addEventListener('click', addTodo);
-
-// Création du tableau
-const table = document.createElement('table');
-const tr = document.createElement('tr');
-const td = document.createElement('td');
-
-// Appliquer la largeur à la cellule
-table.style.border = '1px solid black';
-td.style.width = '10px';
-
-// Ajouter les elements les uns aux autres
-tr.appendChild(td);
-table.appendChild(tr);
-document.body.appendChild(table);
